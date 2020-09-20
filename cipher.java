94822f796126c0a685b42243266fa7a7c7bb0e43
doob265 <48141197+doob265@users.noreply.github.com>
2020-09-20 12:37:03 -0400

    Update cipher.java

--- cipher.java
+++ cipher.java
@@ -1,17 +1,47 @@
-//Mark Dubin, 9/3/20, Cipher Collection
+//Mark Dubin, 9/20/20, Cipher Collection
 import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
 
 //class declaration
 public class cipher{
+    //number of words in topWords.txt
+    final public static int MAXWORDS = 996;
+    //for hill brute force decryption
+    final public static int COUNT = 20;
+
+    //get list of top words from file
+    public static String[] readFromFile() throws IOException{
+        //given there are exactly 1000 words to choose from
+        String[] list = new String[MAXWORDS];
+        int i;
+        //initialize file, scanner, iterating variables
+        File input = new File("topWords.txt");
+        Scanner s = new Scanner(input);
+        //get each word
+        for(i = 0; i < MAXWORDS; i++){
+            list[i] = s.nextLine();
+        }
+
+        //convert each word to uppercase
+        for(i = 0; i < MAXWORDS; i++){
+            list[i] = list[i].toUpperCase();
+        }
+
+        //close scanner and return list
+        s.close();
+        return list;
+    }
+
     //menu function, gets user input for what encryption/decryption they want to perform
     public static int menu(Scanner scan){
         String choice;
         int cho;
-        System.out.println("Press 1 to encrypt using Caesar's Cipher, 2 to decrypt using Caesar's Cipher, 3 to encrypt using the Vigenère Cipher, 4 to decrypt using the Vigenère Cipher, 5 to encrpyt using the Affine Cipher, 6 to decrypt using the Affine Cipher, or 7 to exit.");
+        System.out.println("Press 1 to encrypt using Caesar's Cipher, 2 to decrypt using Caesar's Cipher, 3 to encrypt using the Vigenère Cipher, 4 to decrypt using the Vigenère Cipher, 5 to encrpyt using the Affine Cipher, 6 to decrypt using the Affine Cipher, 7 to decrypt using a 2x2 Hill Cipher or 8 to exit.");
         choice = scan.nextLine();
         //ensures valid input
         while(!menuCheck(choice)){
-            System.out.println("Sorry, please try again. Press 1 to encrypt using Caesar's Cipher, 2 to decrypt using Caesar's Cipher, 3 to encrypt using the Vigenère Cipher, 4 to decrypt using the Vigenère Cipher, 5 to encrpyt using the Affine Cipher, 6 to decrypt using the Affine Cipher, or 7 to exit.");
+            System.out.println("Sorry, please try again. Press 1 to encrypt using Caesar's Cipher, 2 to decrypt using Caesar's Cipher, 3 to encrypt using the Vigenère Cipher, 4 to decrypt using the Vigenère Cipher, 5 to encrpyt using the Affine Cipher, 7 to decrypt using a 2x2 Hill Cipher or 8 to exit.");
             choice = scan.nextLine();
         }
         //convert to int and return
@@ -23,7 +53,7 @@ public class cipher{
     public static boolean menuCheck(String choice){
         int i;
         for(i = 0; i < choice.length(); i++){
-            if(choice.codePointAt(i) < 49 || choice.codePointAt(i) > 55){
+            if(choice.codePointAt(i) < 49 || choice.codePointAt(i) > 56){
                 return false;
             }
         }
@@ -266,17 +296,42 @@ public class cipher{
         word = ret;
         return word;
     }
+
+    //performs Hill Cipher Decryption
+    public static String hillDecrypt(String cipher, int[][] key){
+        char[] plain = new char[cipher.length()];
+        int i, j, sum;
+        //two rows
+        for(i = 0; i < 2; i++){
+            //start at 0 each time encrypting
+            sum = 0;
+            //two columns
+            for(j = 0; j < 2; j++){
+                //System.out.println(cipher.charAt((j)));
+                //multiply the letter by each value in column of key, add these together
+                sum += (key[i][j]*(cipher.charAt((j))-'A'));
+            }
+            //get letter into appropriate range
+            plain[i] = (char)((sum%26)+'A');
+        }
+        //turn plain into a String, return it
+        return new String(plain);
+    }
     
     //main driver function
-    public static void main(String[]args){
-        int key, choice = 0, aa, bru, i, j;
-        String word, temp, k, a, b, brute;
+    public static void main(String[]args) throws IOException{
+        int key, choice = 0, aa, bru, i, j, k, l, m, n, o, len, count;
+        //matrix
+        int[][] matrix = new int[2][2];
+        String word, temp, kk, a, b, brute, plain = "", cipher;
+        //get wordlist from file
+        String[] words = readFromFile();
         Scanner scan = new Scanner(System.in);
         int[] validA = {1,3,5,7,9,11,15,17,19,21,23,25};
 
         //while loop used to continually execute until user is done with program
         System.out.print("Welcome! ");
-        while(choice != 7){
+        while(choice != 8){
             choice = menu(scan);
             //caesar encrypt
             if(choice == 1){
@@ -386,19 +441,19 @@ public class cipher{
                 }
 
                 System.out.println("Enter key to use for encryption, only entering alphabetic characters a-z. If you're using multiple words, don't use spaces between them.");
-                k = scan.nextLine();
-                k = k.toLowerCase();
+                kk = scan.nextLine();
+                kk = kk.toLowerCase();
         
                 //ensure user input is valid
-                while(!strCheck(k, 1, 0)){
+                while(!strCheck(kk, 1, 0)){
                     System.out.println("Sorry, please only enter alphabetical letters a-z. If you're using multiple words, don't use spaces between them.");
-                    k = scan.nextLine();
-                    k.toLowerCase();
+                    kk = scan.nextLine();
+                    kk.toLowerCase();
                 }
 
                 //perform encryption
-                System.out.println("Encrypting " + word + ", using the key " + k + "."); 
-                word = vigenereEncrypt(word, k);
+                System.out.println("Encrypting " + word + ", using the key " + kk + "."); 
+                word = vigenereEncrypt(word, kk);
                 System.out.println("Encryption: " + word);
             }
 
@@ -416,21 +471,22 @@ public class cipher{
                 }
 
                 System.out.println("Enter key to use for decryption, only entering alphabetic characters a-z. If you're using multiple words, don't use spaces between them.");
-                k = scan.nextLine();
-                k = k.toLowerCase();
+                kk = scan.nextLine();
+                kk = kk.toLowerCase();
         
                 //ensure valid input
-                while(!strCheck(k, 1, 0)){
+                while(!strCheck(kk, 1, 0)){
                     System.out.println("Sorry, please only enter alphabetical letters a-z. If you're using multiple words, don't use spaces between them.");
-                    k = scan.nextLine();
-                    k.toLowerCase();
+                    kk = scan.nextLine();
+                    kk.toLowerCase();
                 }
 
                 //perform decryption
-                System.out.println("Decrypting " + word + ", using the key " + k + "."); 
-                word = vigenereDecrypt(word, k);
+                System.out.println("Decrypting " + word + ", using the key " + kk + "."); 
+                word = vigenereDecrypt(word, kk);
                 System.out.println("Decryption: " + word);
             }
+
             //affine encrypt
             else if(choice == 5){
                 System.out.println("Enter word or string to encrypt, only entering alphabetic characters a-z.");
@@ -444,21 +500,21 @@ public class cipher{
                     word.toLowerCase();
                 }
         
-                System.out.println("Enter a, please only enter digits 0-9, and a positive number coprime to 26...");
+                System.out.println("Enter a, please only enter digits 0-9...");
                 a = scan.nextLine();
 
                 //ensure user input is valid
                 while(!intCheck(a)){
-                    System.out.println("Sorry, please only enter numerical digits 0-9, and a positive number coprime to 26...");
+                    System.out.println("Sorry, please only enter numerical digits 0-9...");
                     a = scan.nextLine();
                 }
 
-                System.out.println("Enter b, please only enter digits 0-9, and a positive number coprime to 26...");
+                System.out.println("Enter b, please only enter digits 0-9...");
                 b = scan.nextLine();
 
                 //ensure user input is valid
                 while(!intCheck(b)){
-                    System.out.println("Sorry, please only enter numerical digits 0-9, and a positive number coprime to 26...");
+                    System.out.println("Sorry, please only enter numerical digits 0-9...");
                     temp = scan.nextLine();
                 }
 
@@ -473,6 +529,7 @@ public class cipher{
                 word = affineEncrypt(word, aa, key);
                 System.out.println("Encryption: " + word);
             }
+
             //affine decrypt
             else if(choice == 6){
                 //check if user wants to brute force, or if they have a and b ready
@@ -498,21 +555,21 @@ public class cipher{
         
                 //key ready
                 if(bru == 1){
-                    System.out.println("Enter a, please only enter digits 0-9, and a positive number coprime to 26...");
+                    System.out.println("Enter a, please only enter digits 0-9...");
                     a = scan.nextLine();
     
                     //ensure user input is valid
                     while(!intCheck(a)){
-                        System.out.println("Sorry, please only enter numerical digits 0-9, and a positive number coprime to 26...");
+                        System.out.println("Sorry, please only enter numerical digits 0-9...");
                         a = scan.nextLine();
                     }
     
-                    System.out.println("Enter b, please only enter digits 0-9, and a positive number coprime to 26...");
+                    System.out.println("Enter b, please only enter digits 0-9...");
                     b = scan.nextLine();
     
                     //ensure user input is valid
                     while(!intCheck(b)){
-                        System.out.println("Sorry, please only enter numerical digits 0-9, and a positive number coprime to 26...");
+                        System.out.println("Sorry, please only enter numerical digits 0-9...");
                         temp = scan.nextLine();
                     }
     
@@ -553,9 +610,71 @@ public class cipher{
                         i = validA.length + 1;
                     }
                 }
-                }
             }
         }
+        else if(choice == 7){
+        //get ciphertext from user, convert to uppercase, store it's length
+        System.out.println("Please enter the message you want to decrypt.");
+        cipher = scan.nextLine();
+        cipher = cipher.toUpperCase();
+        len = cipher.length();
+
+        //outermost for loop
+        for(i = 0; i < 26; i++){
+            for(j = 0; j < 26; j++){
+                for(k = 0; k < 26; k++){
+                    //innermost for loop
+                    for(l = 0; l < 26; l++){
+                        //this value will continually get increased for each unique matrix, or 26^4 times
+                        matrix[1][1]++;
+                        matrix[1][1] %= 26;
+                        //reset plain
+                        plain = "";
+                        //convert ciphertext to plaintext
+                        for(m = 0; m< len; m+=2){
+                            //System.out.println(i);
+                            plain = plain.concat(hillDecrypt(cipher.substring(m, m + 2), matrix));
+                        }
+
+                        //reset count
+                        count = 0;
+                        //go through entirety of top words array
+                        for(m = 0; m < words.length; m++){
+                            //check if each word is substring of plaintext
+                            if(plain.contains(words[m])){
+                                count++;
+                                //if count reaches above target value, print out cooresponding info and terminate
+                                if(count > COUNT){
+                                    //print plaintext
+                                    System.out.println(plain);
+                                    //print key
+                                    for(n = 0; n < 2; n++){
+                                        for(o = 0; o < 2; o++){
+                                            System.out.print(matrix[n][o] + " ");
+                                        }
+                                        System.out.println();
+                                    }
+                                    System.out.println("This should be right. If not, increase the count variable in the code, and try again.");
+                                    m = words.length;
+                                }
+                            }
+                        }
+                    }
+                    //this value will get increased every time the innermost value rolls over, or 26^3 times
+                    matrix[0][1]++;
+                    matrix[0][1] %= 26;
+                }
+                //this value will get increased 26^2 times
+                matrix[1][0]++;
+                matrix[1][0] %= 26;
+            }
+            //the outermost value will increase 26 times
+            matrix[0][0]++;
+            matrix[0][0] %= 26;
+        }
+        System.out.println("Message not found. Increase the count variable in the code, and try again.");
+        }
+    }
 
         //user is done, close scanner 
         System.out.println("Have a good day!");
